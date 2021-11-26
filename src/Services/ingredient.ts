453005d834a4938f069e955e4c8d93a06c974f63
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2021-11-26 14:37:14 +0700

    change min length string from 6 to 3

--- src/Services/ingredient.ts
+++ src/Services/ingredient.ts
@@ -1,10 +1,10 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Joi from "joi";
 
 import db from "Loaders/sequelize";
-import {sendRes} from "Utils/api_util/";
-import {UnitEnum} from "Utils/enum/";
-import {INGREDIENT_LOG, SERVER_LOG} from "Utils/response_message/";
+import { sendRes } from "Utils/api_util/";
+import { UnitEnum } from "Utils/enum/";
+import { INGREDIENT_LOG, SERVER_LOG } from "Utils/response_message/";
 
 const options = {
 	allowUnknown: true, // ignore unknown props
@@ -27,7 +27,7 @@ export const get_ingredients = async (_: Request, res: Response) => {
 
 export const get_ingredient = async (req: Request, res: Response) => {
 	try {
-		const {ingredient_id} = req.params;
+		const { ingredient_id } = req.params;
 
 		const ingredient = await Ingredient.findByPk(ingredient_id);
 
@@ -42,7 +42,7 @@ export const get_ingredient = async (req: Request, res: Response) => {
 export const create_ingredient = async (req: Request, res: Response) => {
 	try {
 		const schema = Joi.object({
-			name: Joi.string().min(6).required(),
+			name: Joi.string().min(3).required(),
 			stock: Joi.number().min(0).required(),
 			unit: Joi.string()
 				.valid(
@@ -55,15 +55,15 @@ export const create_ingredient = async (req: Request, res: Response) => {
 				.required(),
 		});
 
-		const {error} = schema.validate(req.body, options);
+		const { error } = schema.validate(req.body, options);
 
-		const {name, stock, unit} = req.body;
+		const { name, stock, unit } = req.body;
 		let ingredient = new Ingredient();
 
 		if (error)
 			return sendRes(res, 400, INGREDIENT_LOG.POST[400].BAD_REQUEST, error);
 
-		ingredient = await Ingredient.create({name, stock, unit});
+		ingredient = await Ingredient.create({ name, stock, unit });
 
 		return sendRes(res, 200, INGREDIENT_LOG.POST[200], ingredient);
 	} catch (err) {
@@ -73,13 +73,13 @@ export const create_ingredient = async (req: Request, res: Response) => {
 
 export const update_ingredient = async (req: Request, res: Response) => {
 	try {
-		const {ingredient_id} = req.params;
+		const { ingredient_id } = req.params;
 		const ingredient = await Ingredient.findByPk(ingredient_id);
 
 		if (!ingredient) return sendRes(res, 404, INGREDIENT_LOG.GET[404]);
 
 		const schema = Joi.object({
-			name: Joi.string().min(6),
+			name: Joi.string().min(3),
 			stock: Joi.number().min(0),
 			unit: Joi.string().valid(
 				UnitEnum.gram,
@@ -90,12 +90,12 @@ export const update_ingredient = async (req: Request, res: Response) => {
 			),
 		});
 
-		const {error} = schema.validate(req.body, options);
+		const { error } = schema.validate(req.body, options);
 
 		if (error)
 			return sendRes(res, 400, INGREDIENT_LOG.PUT[400].BAD_REQUEST, error);
 
-		const {name, stock, unit} = req.body;
+		const { name, stock, unit } = req.body;
 
 		ingredient.name = name ? name : ingredient.name;
 		ingredient.stock = stock ? stock : ingredient.stock;
